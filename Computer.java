75a0ba1d251dd36c895fabf5ab9551fea9af5d21
m1m22a23 <m1m22a23.oit.ac.jp>
2023-12-22 10:11:33 +0900

    User_2

--- Computer.java
+++ Computer.java
@@ -2,9 +2,10 @@ import java.util.Random;
 
 public class Computer {
   public static int decidesComputerHand() {
-	//課題3 Computerの手の決定とその表示
-	//Randomメソッドを使って1~3の数値を返す
-	
-	
+    // 課題3 Computerの手の決定とその表示
+    // Randomメソッドを使って1~3の数値を返す
+    Random random = new Random();
+    int rnd = random.nextInt(3) + 1;
+    return rnd;
   }
 }
--- Hand.java
+++ Hand.java
@@ -2,15 +2,19 @@ public class Hand {
   public static final int GU = 1;
   public static final int CHOKI = 2;
   public static final int PA = 3;
+
   public static String getHandName(int hand) {
-	  String handName = "";
-	
-	  //課題2 Playerの手の決定とその表示
-	  //1ならば"グー",2ならば"チョキ",3ならば"パー"を返す
-	  if (hand == GU) handName = "グー";
-    else if (hand == CHOKI) handName = "チョキ";
-    else if (hand == PA) handName = "パー";
-	
-	  return handName;
+    String handName = "";
+
+    // 課題2 Playerの手の決定とその表示
+    // 1ならば"グー",2ならば"チョキ",3ならば"パー"を返す
+    if (hand == GU)
+      handName = "グー";
+    else if (hand == CHOKI)
+      handName = "チョキ";
+    else if (hand == PA)
+      handName = "パー";
+
+    return handName;
   }
 }
