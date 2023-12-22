2de16850f744bac75fe419637d6182248a43f42e
SyuPyGitHubTest <130703590+SyuPyGitHubTest@users.noreply.github.com>
2023-12-22 09:07:41 +0900

    GCBot:GameMain.javaを編集

--- GameMain.java
+++ GameMain.java
@@ -1,21 +1,21 @@
 public class GameMain {
-  public static void main(String[] args) {
-    // 課題1 文章の表示
-    System.out.println("1:グー 2:チョキ 3:パー");
-    System.out.println("じゃんけん...");
+	public static void main(String[] args) {
+		// 課題1 文章の表示
+		System.out.println("1:グー 2:チョキ 3:パー");
+		System.out.println("じゃんけん...");
 
-    // 課題2 Playerの手の決定とその表示
-    // PlayerクラスのdecidesPlayerHandメソッドとHandクラスのgetHandNameメソッドを使ってPlayerの手を表示する
+		// 課題2 Playerの手の決定とその表示
+		// PlayerクラスのdecidesPlayerHandメソッドとHandクラスのgetHandNameメソッドを使ってPlayerの手を表示する
+		int playerHand = Player.decidesPlayerHand();
+		System.out.println("プレイヤー : " + Hand.getHandName(playerHand));
 
-    // int playerHand =
+		// 課題3 Computerの手の決定とその表示
+		// ComputerクラスのdecidesComputerHandメソッドとHandクラスのgetHandNameメソッドを使ってPlayerの手を表示する
 
-    // 課題3 Computerの手の決定とその表示
-    // ComputerクラスのdecidesComputerHandメソッドとHandクラスのgetHandNameメソッドを使ってPlayerの手を表示する
+		// int computerHand =
 
-    // int computerHand =
+		// 課題4 勝敗判定とその表示
+		// 課題2,3において実装したPlayerおよびComputerの手の表示はVictoryOrDefeatクラスに移動する
 
-    // 課題4 勝敗判定とその表示
-    // 課題2,3において実装したPlayerおよびComputerの手の表示はVictoryOrDefeatクラスに移動する
-
-  }
+	}
 }
